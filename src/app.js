eb3abaef7cea2e6d7475ea56e3116b8201486fc9
jaboulos <jab12288@gmail.com>
2021-01-10 10:40:46 -0800

    Webpack setup

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import reactDOM from "react-dom";
+import "./styles/style.scss";
+
+const App = () => {
+  return <h1>Hello Reviews Service</h1>;
+};
+
+reactDOM.render(<App />, document.getElementById("root"));
