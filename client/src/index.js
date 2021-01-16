7563cf782a1de2581082f48bf66742cdc351c6c4
jaboulos <jab12288@gmail.com>
2021-01-16 11:48:14 -0800

    Update webpack config to use semantic ui

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./styles/style.scss";
+
 import "./styles/style.css";
 import "semantic-ui-css/semantic.min.css";
-// import "../../node_modules/semantic-ui-css/semantic.min.css";
-// import "semantic-ui-css/semantic.js";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./client/src/app.js",
+  entry: "./client/src/index.js",
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js",
@@ -13,14 +13,25 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader",
       },
-      // {
-      //   test: /\.css$/,
-      //   use: ['style-loader', 'css-loader']
-      // }
       {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpg|woff|svg|eot|ttf|woff2)$/,
+        loader: "url-loader?limit=8192&name=images/[name].[ext]",
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+        },
+      },
+      // {
+      //   test: /\.scss$/,
+      //   use: ["style-loader", "css-loader", "sass-loader"],
+      // },
     ],
   },
   devtool: "cheap-module-eval-source-map",
